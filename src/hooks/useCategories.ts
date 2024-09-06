db6699c1aec3c2542a12d5e66dc1241b960deea7
abyayel <abyayel6@gmail.com>
2024-09-06 17:12:22 +0300

    Concatenate two category arrays

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -9,9 +9,26 @@ interface Categories {
 }
 
 function useCategories() {
-  return useData<Categories>("/categories", {
-    params: { sort: "-totalMediaCount" },
+  const {
+    data: data1,
+    error: error1,
+    isLoading: isLoading1,
+  } = useData<Categories>("/categories", {
+    params: { sort: "-totalMediaCount", "page[limit]": 20, "page[offset]": 0 },
   });
+  const {
+    data: data2,
+    error: error2,
+    isLoading: isLoading2,
+  } = useData<Categories>("/categories", {
+    params: { sort: "-totalMediaCount", "page[limit]": 20, "page[offset]": 20 },
+  });
+
+  const data = data1.concat(data2);
+  const error = error1 && error2;
+  const isLoading = isLoading1 && isLoading2;
+
+  return { data, isLoading, error };
 }
 
 export { useCategories };
