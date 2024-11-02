d76bbd7b52c53ce259e96313736d735f2f9c81c9
abyayel <abyayel6@gmail.com>
2024-11-02 12:57:08 +0300

    Build a responsive layout for anime details page

--- src/pages/AnimeDetailPage.tsx
+++ src/pages/AnimeDetailPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useAnime } from "../hooks/useAnime";
-import { Heading, Spinner } from "@chakra-ui/react";
+import { Grid, GridItem, Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandabaleText";
 
 function AnimeDetailsPage() {
@@ -13,8 +13,43 @@ function AnimeDetailsPage() {
 
   return (
     <div>
-      <Heading>{title}</Heading>
-      <ExpandableText>{description}</ExpandableText>
+      <Grid
+        templateAreas={{
+          base: `"poster" "genres" "title" "description" "info" `,
+          md: `"poster title" "poster genres" "poster description" "poster info"`,
+          xl: `"poster title info" "poster genres info" "poster description info"`,
+        }}
+      >
+        <GridItem area="poster" backgroundColor="red">
+          Poster
+        </GridItem>
+        <GridItem area="title" backgroundColor="blue">
+          <Heading>{title}</Heading>
+        </GridItem>
+        <GridItem area="genres" backgroundColor="lime">
+          Genres
+        </GridItem>
+        <GridItem area="description" backgroundColor="green">
+          <ExpandableText>{description}</ExpandableText>
+        </GridItem>
+        <GridItem area="info" backgroundColor="purple">
+          Info
+        </GridItem>
+      </Grid>
+
+      <Grid
+        templateAreas={{
+          base: `"episodes" "details"`,
+          md: `"episodes details"`,
+        }}
+      >
+        <GridItem area="episodes" backgroundColor="orange">
+          Episodes
+        </GridItem>
+        <GridItem area="details" backgroundColor="gold">
+          Episode details
+        </GridItem>
+      </Grid>
     </div>
   );
 }
