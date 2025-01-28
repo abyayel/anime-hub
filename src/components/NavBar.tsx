bdd8a9b52d5e9c30094ef52cb108931518f8b26e
abyayel <abyayel6@gmail.com>
2025-01-29 02:12:35 +0300

    Refactor - correct navigation for logo in anime detail page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,14 +3,17 @@ import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import Searchinput from "./Searchinput";
 import useAnimeQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
   const onSearch = useAnimeQueryStore((store) => store.setSearchText);
+  const navigate = useNavigate();
   return (
     <HStack paddingX={5} paddingY={3} backgroundColor="blue.500">
       <Image
         onClick={() => {
           onSearch(undefined);
+          navigate("/");
         }}
         boxSize="60px"
         src={logo}
